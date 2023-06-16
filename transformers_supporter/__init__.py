5be5839952768c68e4bd9783d31f1bfc934490de
Sang Ki Kwon <108699325+automatethem@users.noreply.github.com>
2023-06-17 00:00:05 +0900

    Update __init__.py

--- transformers_supporter/__init__.py
+++ transformers_supporter/__init__.py
@@ -4,9 +4,14 @@ from . import pipelines
 from .models.ann import configuration_ann
 from .models.ann import feature_extraction_ann
 from .models.ann import modeling_ann
+
 from .models.cnn import configuration_cnn
 from .models.cnn import modeling_cnn
 from .models.cnn import image_processing_cnn
+
+from .models.custom_bert import configuration_custom_bert
+from .models.custom_bert import modeling_custom_bert
+
 def register_auto():
     configuration_ann.register_auto()
     modeling_ann.register_auto()
@@ -16,3 +21,5 @@ def register_auto():
     modeling_cnn.register_auto()
     image_processing_cnn.register_auto()
 
+    configuration_custom_bert.register_auto()
+    modeling_custom_bert.register_auto()
