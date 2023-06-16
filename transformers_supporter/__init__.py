ee31c90c66edb0810ec39cad150a36243fcbed9d
Sang Ki Kwon <108699325+automatethem@users.noreply.github.com>
2023-06-16 23:48:49 +0900

    Update __init__.py

--- transformers_supporter/__init__.py
+++ transformers_supporter/__init__.py
@@ -1,12 +1,6 @@
 from . import models
 from . import pipelines
 
-from transformers_supporter.models.cnn.configuration_cnn import CnnConfig
-from transformers_supporter.models.cnn.modeling_cnn import CnnForImageClassification
-
-from transformers_supporter.models.ann.configuration_ann import AnnConfig
-from transformers_supporter.models.ann.modeling_ann import CnnForImageClassification
-
 from .models.ann import modeling_ann
 from .models.cnn import configuration_cnn
 from .models.cnn import modeling_cnn
