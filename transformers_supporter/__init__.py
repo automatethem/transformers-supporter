25c947f43d98d0574a4196337b4bd84044c8fdbe
Sang Ki Kwon <108699325+automatethem@users.noreply.github.com>
2023-06-17 00:42:48 +0900

    Update __init__.py

--- transformers_supporter/__init__.py
+++ transformers_supporter/__init__.py
@@ -28,6 +28,12 @@ from .models.faster_rcnn import image_processing_faster_rcnn
 from .models.rnn import configuration_rnn
 from .models.rnn import modeling_rnn
 
+from .pipelines import tabular_regression_pipeline
+from .pipelines import tabular_binary_classification_pipeline
+from .pipelines import tabular_classification_pipeline
+from .pipelines import custom_image_classification_pipeline
+from .pipelines import fixed_length_translation_pipeline
+
 def register_auto():
     configuration_ann.register_auto()
     modeling_ann.register_auto()
@@ -55,3 +61,9 @@ def register_auto():
 
     configuration_rnn.register_auto()
     modeling_rnn.register_auto()
+
+    tabular_regression_pipeline.register_pipeline()
+    tabular_binary_classification_pipeline.register_pipeline()
+    tabular_classification_pipeline.register_pipeline()
+    custom_image_classification_pipeline.register_pipeline()
+    fixed_length_translation_pipeline.register_pipeline()
