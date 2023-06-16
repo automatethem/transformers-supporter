f11e950a929aadcee233b73fed35960eaf21d5a1
Sang Ki Kwon <108699325+automatethem@users.noreply.github.com>
2023-06-16 23:40:42 +0900

    Update configuration_ann.py

--- transformers_supporter/models/ann/configuration_ann.py
+++ transformers_supporter/models/ann/configuration_ann.py
@@ -15,3 +15,6 @@ class AnnConfig(PretrainedConfig):
         #https://github.com/huggingface/transformers/blob/98d88b23f54e5a23e741833f1e973fdf600cc2c5/src/transformers/configuration_utils.py#L323
         #Keys are always strings in JSON so convert ids to int here.
         super().__init__(**kwargs)
+
+def register_auto():
+    AutoConfig.register("ann", AnnConfig)
