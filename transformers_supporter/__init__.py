4ff05bb5996143c9217f539feb3130734a7e585f
Sang Ki Kwon <108699325+automatethem@users.noreply.github.com>
2023-06-16 23:42:51 +0900

    Update __init__.py

--- transformers_supporter/__init__.py
+++ transformers_supporter/__init__.py
@@ -3,7 +3,15 @@ from . import pipelines
 
 from transformers_supporter.models.cnn.configuration_cnn import CnnConfig
 from transformers_supporter.models.cnn.modeling_cnn import CnnForImageClassification
+
+from transformers_supporter.models.ann.configuration_ann import AnnConfig
+from transformers_supporter.models.ann.modeling_ann import CnnForImageClassification
+
+from .models.ann import modeling_ann
+
 def register_auto():
     AutoConfig.register("cnn", CnnConfig)
     AutoModelForImageClassification.register(CnnConfig, CnnForImageClassification)
   
+
+    modeling_ann.register_auto()
