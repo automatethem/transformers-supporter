e7a99422156f8e62e31deb6b4e20722119755188
Sang Ki Kwon <108699325+automatethem@users.noreply.github.com>
2023-06-17 15:00:40 +0900

    Update modeling_cnn.py

--- transformers_supporter/models/cnn/modeling_cnn.py
+++ transformers_supporter/models/cnn/modeling_cnn.py
@@ -6,7 +6,7 @@ from transformers import AutoConfig
 from transformers import AutoModelForImageClassification
 import torch
 from torch.nn import functional as F
-#import pytorch_supporter
+import pytorch_supporter
 import transformers
 from .configuration_cnn import CnnConfig
     
@@ -25,8 +25,8 @@ class CnnForImageClassification(PreTrainedModel):
             torch.nn.ReLU(),
             torch.nn.MaxPool2d(kernel_size=2, stride=2, padding=0),
             torch.nn.Flatten(), #배치를 제외한 모든 차원을 평탄화
-            #pytorch_supporter.layers.LazilyInitializedLinear(out_features=config.num_labels)
-            torch.nn.Linear(in_features=(512 if config.in_channels==1 else 89888), out_features=config.num_labels)  
+            #torch.nn.Linear(in_features=(512 if config.in_channels==1 else 89888), out_features=config.num_labels)  
+            pytorch_supporter.layers.LazilyInitializedLinear(out_features=config.num_labels)
         )
 
     def forward(self, pixel_values, labels=None):
