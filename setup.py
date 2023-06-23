97a0ffd652aa14fa875f1adc0272ed80c75daffd
Sang Ki Kwon <108699325+automatethem@users.noreply.github.com>
2023-06-23 23:40:38 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def requirements():
 
 setuptools.setup(
 	name='transformers-supporter',
-	version='0.0.26',
+	version='0.0.27',
 	description='Transformers supporter',
 	long_description=open('README.md').read(),
 	long_description_content_type='text/markdown',
