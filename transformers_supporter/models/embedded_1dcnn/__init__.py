b4eb565727462f0fd06fd2e5b8caee14054aa20d
Sang Ki Kwon <108699325+automatethem@users.noreply.github.com>
2023-06-25 19:37:17 +0900

    Update __init__.py

--- transformers_supporter/models/embedded_1dcnn/__init__.py
+++ transformers_supporter/models/embedded_1dcnn/__init__.py
@@ -2,5 +2,5 @@ from . import configuration_embedded_1dcnn
 from . import modeling_embedded_1dcnn
 
 def register():
-    configuration_embedded_1dcnn.register_auto()
-    modeling_embedded_1dcnn.register_auto()
+    configuration_embedded_1dcnn.register()
+    modeling_embedded_1dcnn.register()
