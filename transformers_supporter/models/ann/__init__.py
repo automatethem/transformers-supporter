3e3d0c78642c6feb452be9ad25fe6ee32118d98c
Sang Ki Kwon <108699325+automatethem@users.noreply.github.com>
2023-06-25 19:34:46 +0900

    Update __init__.py

--- transformers_supporter/models/ann/__init__.py
+++ transformers_supporter/models/ann/__init__.py
@@ -2,7 +2,7 @@ from . import configuration_ann
 from . import modeling_ann
 from . import feature_extraction_ann
 
-def register_auto():
+def register():
     configuration_ann.register_auto()
     modeling_ann.register_auto()
     feature_extraction_ann.register_auto()
