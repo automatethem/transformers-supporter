f4589659859577021d5b0c0dbd22daf41aa7427f
Sang Ki Kwon <108699325+automatethem@users.noreply.github.com>
2023-06-25 19:33:31 +0900

    Update __init__.py

--- transformers_supporter/models/cnn/__init__.py
+++ transformers_supporter/models/cnn/__init__.py
@@ -2,7 +2,7 @@ from . import configuration_cnn
 from . import modeling_cnn
 from . import image_processing_cnn
 
-def register_auto():
-    configuration_cnn.register_auto()
-    modeling_cnn.register_auto()
-    image_processing_cnn.register_auto()
+def register():
+    configuration_cnn.register()
+    modeling_cnn.register()
+    image_processing_cnn.register()
