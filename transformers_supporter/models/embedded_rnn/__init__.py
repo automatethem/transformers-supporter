abbe99c616ce471a4b1f99aea56557dbd484ab7b
Sang Ki Kwon <108699325+automatethem@users.noreply.github.com>
2023-06-25 19:37:58 +0900

    Update __init__.py

--- transformers_supporter/models/embedded_rnn/__init__.py
+++ transformers_supporter/models/embedded_rnn/__init__.py
@@ -2,7 +2,7 @@ from . import configuration_embedded_rnn
 from . import modeling_embedded_rnn
 from . import feature_extraction_embedded_rnn
 
-def register_auto():
-    configuration_embedded_rnn.register_auto()
-    modeling_embedded_rnn.register_auto()
-    feature_extraction_embedded_rnn.register_auto()
+def register():
+    configuration_embedded_rnn.register()
+    modeling_embedded_rnn.register()
+    feature_extraction_embedded_rnn.register()
