7e150626328aa8af95282a2a8f30a7617fe4948e
Sang Ki Kwon <108699325+automatethem@users.noreply.github.com>
2023-06-25 19:40:27 +0900

    Update __init__.py

--- transformers_supporter/models/rnn/__init__.py
+++ transformers_supporter/models/rnn/__init__.py
@@ -2,7 +2,7 @@ from . import configuration_rnn
 from . import modeling_rnn
 from . import feature_extraction_rnn
 
-def register_auto():
-    configuration_rnn.register_auto()
-    modeling_rnn.register_auto()
-    feature_extraction_rnn.register_auto()
+def register():
+    configuration_rnn.register()
+    modeling_rnn.register()
+    feature_extraction_rnn.register()
