fa27a79b81ef68709d6355fd89f6b73b0539c1f4
Sang Ki Kwon <108699325+automatethem@users.noreply.github.com>
2023-06-25 19:36:31 +0900

    Update __init__.py

--- transformers_supporter/models/custom_bert/__init__.py
+++ transformers_supporter/models/custom_bert/__init__.py
@@ -1,6 +1,6 @@
 from . import configuration_custom_bert
 from . import modeling_custom_bert
 
-def register_auto():
-    configuration_custom_bert.register_auto()
-    modeling_custom_bert.register_auto()
+def register():
+    configuration_custom_bert.register()
+    modeling_custom_bert.register()
